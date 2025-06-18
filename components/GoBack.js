7d3c4d79a2379f8dea7cc89410e1b4ad2ac4f7db
arjav0703 <arjavjain0703@gmail.com>
2025-06-18 15:38:00 +0530

    use next/link

--- components/GoBack.js
+++ components/GoBack.js
@@ -1,9 +1,11 @@
 import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function GoBack({url}) {
     return(
-        <a href={url} className="active:scale-90 transition-all top-10 left-10 fixed text-white">
+        <Link href={url} className="active:scale-90 transition-all top-10 left-10 fixed text-white">
             <ChevronLeft size={50}/>
-        </a>
+        </Link>
+        
     )
 }
--- components/MyButton.js
+++ components/MyButton.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function MyButton({children, url}) {
     return(
-        <a href={url}>
+        <Link href={url}>
             <button className="p-3 text-lg transition-all border-white border-1 rounded-lg bg-transparent backdrop-blur-2xl backdrop-brightness-75 hover:scale-110 active:scale-75 text-white">
                 {children}
             </button>
-        </a>
+        </Link>
     )
 }
